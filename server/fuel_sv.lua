7ae064e3b9c6fb8e4568bdbc8a55a3942b234dad
Shorty <94754507+Sh0rtyyy@users.noreply.github.com>
2024-06-02 13:45:26 +0200

    Update fuel_sv.lua

--- server/fuel_sv.lua
+++ server/fuel_sv.lua
@@ -140,24 +140,3 @@ end)
 RegisterNetEvent('cdn-syphoning:callcops', function(coords)
     TriggerClientEvent('cdn-syphoning:client:callcops', -1, coords)
 end)
-
---- Update Alerts
-local updatePath
-local resourceName
-
-local function checkVersion(err, responseText, headers)
-    local curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
-	if responseText == nil then print("^1"..resourceName.." check for updates failed ^7") return end
-    if curVersion ~= nil and responseText ~= nil then
-		if curVersion == responseText then Color = "^2" else Color = "^1" end
-        print("\n^1----------------------------------------------------------------------------------^7")
-        print(resourceName.."'s latest version is: ^2"..responseText.."!\n^7Your current version: "..Color..""..curVersion.."^7!\nIf needed, update from https://github.com"..updatePath.."")
-        print("^1----------------------------------------------------------------------------------^7")
-    end
-end
-
-CreateThread(function()
-	updatePath = "/CodineDev/cdn-fuel"
-	resourceName = "cdn-fuel ("..GetCurrentResourceName()..")"
-	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
-end)
